d2649016e0812dcc098b4ae8f1ecbba47b5c63f6
OlgaLiubar <olgaliubar@yandex.ru>
2021-09-22 05:48:25 +0300

    cors

---
+++ middlewares/cors.js
@@ -0,0 +1,24 @@
+module.exports = (req, res, next) => {
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const ALLOWED_CORS = process.env.ALLOWED_CORS ? process.env.ALLOWED_CORS.split(',') : [
+  'https://olgaliubar.nomoredomains.club',
+  'http://olgaliubar.nomoredomains.club',
+  'http://localhost:3000'
+  ];
+
+  if (ALLOWED_CORS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+
+  next();
+};
